b4ccbda614750a3a79c867ecab58041c9c4a3cef
Elena Soare <2309307s@student.gla.ac.uk>
2019-10-19 12:51:19 +0100

    database2

--- JellyBin02/JellyBin02/JellyBin02/App.xaml.cs
+++ JellyBin02/JellyBin02/JellyBin02/App.xaml.cs
@@ -1,18 +1,33 @@
 ﻿using System;
 using Xamarin.Forms;
+using System.IO;
 using Xamarin.Forms.Xaml;
 
 namespace JellyBin02
 {
     public partial class App : Application
     {
+        static Database database;
+
+        public static Database Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "bin.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new HomePage();
         }
-
+        // When we deploy the project to the phone, the first class which will be looked at is App, and InitializeComponent() initializes the 
+        //main page of the app. This creates a connection to HomePage
         protected override void OnStart()
         {
             // Handle when your app starts
