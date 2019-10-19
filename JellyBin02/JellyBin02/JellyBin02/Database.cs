e0196998f0c084bfc0de29bde56b18afe85e5f27
Elena Soare <2309307s@student.gla.ac.uk>
2019-10-19 12:58:02 +0100

    dabases.cs

---
+++ JellyBin02/JellyBin02/JellyBin02/Database.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace JellyBin02
+{
+    public class Database
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Bin>().Wait();
+        }
+
+        public Task<List<Bin>> GetPeopleAsync()
+        {
+            return _database.Table<Bin>().ToListAsync();
+        }
+
+        public Task<int> SavePersonAsync(Bin person)
+        {
+            return _database.InsertAsync(person);
+        }
+    }
+}
